e764f8b728ca3e3077146197b639cc8e4a89b689
ingrid917 <ingridmatei17@gmail.com>
2026-05-10 22:54:11 +0300

    Upgrade project to .NET 10

--- Siemens.Internship2026.GradeBook/obj/Debug/net8.0/Siemens.Internship2026.GradeBook.AssemblyInfo.cs
+++ Siemens.Internship2026.GradeBook/obj/Debug/net8.0/Siemens.Internship2026.GradeBook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Siemens.Internship2026.GradeBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f18343dfa0b6f0ee5b8594c71eb881dc5b93d558")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+390d6e9c72baf5ad41b4158845d39a5a67b26ca6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Siemens.Internship2026.GradeBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Siemens.Internship2026.GradeBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
